d12dc552cd5c5ae802deba8279fc05ae50838abd
elegantiron <elegant.iron3167@fastmail.com>
2025-10-13 19:41:30 -0400

    add export header

---
+++ inc/libslh/Engine.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "libslh/Engine/Clock.hpp"        // IWYU pragma: export
+#include "libslh/Engine/Core.hpp"         // IWYU pragma: export
+#include "libslh/Engine/RNG.hpp"          // IWYU pragma: export
+#include "libslh/Engine/Scene.hpp"        // IWYU pragma: export
+#include "libslh/Engine/SceneManager.hpp" // IWYU pragma: export
+#include "libslh/Engine/Timer.hpp"        // IWYU pragma: export
+
+/**
+ * @brief Everything you need to run a game.
+ * 
+ */
+namespace libslh::Engine{}
