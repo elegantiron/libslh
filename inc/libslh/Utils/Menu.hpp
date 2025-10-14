fe968fdf9d4cc0b8de1127b2e2d990fdca67de52
elegantiron <elegant.iron3167@fastmail.com>
2025-10-14 18:13:09 -0400

    update Menu

--- inc/libslh/Utils/Menu.hpp
+++ inc/libslh/Utils/Menu.hpp
@@ -1,46 +1,26 @@
 #pragma once
 
+#include "libslh/Types/Font.hpp"
+
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/Text.hpp>
+#include <SFML/Window/Keyboard.hpp>
 
 namespace libslh {
     class Menu : public sf::Drawable {
+        int                   _idx = 0;
         std::vector<sf::Text> _items;
-        sf::Font              _font;
-        unsigned int          _defaultCharacterSize;
-        sf::Color             _defaultFillColor;
-        float                 _defaultOutlineThickness;
-        sf::Color             _defaultOutlineColor;
-        unsigned int          _selectedCharacterSize;
-        sf::Color             _selectedFillColor;
-        float                 _selectedOutlineThickness;
-        sf::Color             _selectedOutlineColor;
+        Font                  _defaultFace;
+        Font                  _selectedFace;
+
+        void updateIndex(int delta);
 
     public:
-        Menu(sf::Font&    font,
-             unsigned int characterSize,
-             sf::Color    defaultFillColor,
-             sf::Color    selectedFillColor);
-        Menu(sf::Font&    font,
-             unsigned int defaultCharacterSize,
-             sf::Color    defaultFillColor,
-             unsigned int selectedCharacterSize,
-             sf::Color    selectedFillColor);
-        Menu(sf::Font&    font,
-             unsigned int defaultCharacterSize,
-             sf::Color    defaultFillColor,
-             float        defaultOutlineThickness,
-             sf::Color    defaultOutlineColor,
-             unsigned int selectedCharacterSize,
-             sf::Color    selectedFillColor,
-             float        selectedOutlineThickness,
-             sf::Color    selectedOutlineColor);
-        void addItem(const sf::String& line);
-        void draw(sf::RenderTarget& target,
-                  sf::RenderStates  states) const override;
+        Menu(Font defaultFace, Font selectedFace);
+        void addItem(sf::String& line);
     };
 }
--- src/Utils/Menu.cpp
+++ src/Utils/Menu.cpp
@@ -1,65 +1,26 @@
 #include "libslh/Utils/Menu.hpp"
 
+namespace {
+    constexpr float SPACING_FACTOR = 1.5F;
+}
+
 namespace libslh {
-    void Menu::addItem(const sf::String& line) {
-        sf::Text newItem{_font};
-        newItem.setString(line);
-        newItem.setCharacterSize(_defaultCharacterSize);
-        newItem.setFillColor(_defaultFillColor);
-        newItem.setOutlineThickness(_defaultOutlineThickness);
+    Menu::Menu(Font defaultFace, Font selectedFace)
+        : _defaultFace(std::move(defaultFace)),
+          _selectedFace(std::move(selectedFace)) {}
+
+    void Menu::addItem(sf::String& line) {
+        _items.push_back(_defaultFace.createText(line));
     }
 
-    Menu::Menu(sf::Font&    font,
-               unsigned int characterSize,
-               sf::Color    defaultFillColor,
-               sf::Color    selectedFillColor)
-        : Menu(font,
-               characterSize,
-               defaultFillColor,
-               0,
-               defaultFillColor,
-               characterSize,
-               selectedFillColor,
-               0,
-               selectedFillColor) {}
-
-    Menu::Menu(sf::Font&    font,
-               unsigned int defaultCharacterSize,
-               sf::Color    defaultFillColor,
-               unsigned int selectedCharacterSize,
-               sf::Color    selectedFillColor)
-        : Menu(font,
-               defaultCharacterSize,
-               defaultFillColor,
-               0,
-               defaultFillColor,
-               selectedCharacterSize,
-               selectedFillColor,
-               0,
-               selectedFillColor) {}
-
-    Menu::Menu(sf::Font&    font,
-               unsigned int defaultCharacterSize,
-               sf::Color    defaultFillColor,
-               float        defaultOutlineThickness,
-               sf::Color    defaultOutlineColor,
-               unsigned int selectedCharacterSize,
-               sf::Color    selectedFillColor,
-               float        selectedOutlineThickness,
-               sf::Color    selectedOutlineColor)
-        : _font(font),
-          _defaultCharacterSize(defaultCharacterSize),
-          _defaultFillColor(defaultFillColor),
-          _defaultOutlineThickness(defaultOutlineThickness),
-          _defaultOutlineColor(defaultOutlineColor),
-          _selectedCharacterSize(selectedCharacterSize),
-          _selectedFillColor(selectedFillColor),
-          _selectedOutlineThickness(selectedOutlineThickness),
-          _selectedOutlineColor(selectedOutlineColor) {}
-
-    void Menu::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-        for (auto item : _items) {
-            target.draw(item);
+    void Menu::updateIndex(int delta) {
+        _defaultFace.configureText(_items[_idx]);
+        _idx += delta;
+        auto numItems = static_cast<int>(_items.size());
+        _idx %= numItems;
+        if (_idx < 0) {
+            _idx = numItems - 1;
         }
+        _selectedFace.configureText(_items[_idx]);
     }
 }
