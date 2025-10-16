0fe7c752a605ab6e684ef70406135f7e7a1e477f
elegantiron <elegant.iron3167@fastmail.com>
2025-10-15 21:23:58 -0400

    start adding imgui-sfml also contains some formatting

--- src/Engine/Core.cpp
+++ src/Engine/Core.cpp
@@ -1,76 +1,89 @@
 #include "libslh/Engine/Core.hpp"
 
+#include <imgui-SFML.h>
+#include <imgui.h>
+
+namespace {
+    constexpr int FRAME_LIMIT = 60;
+}
+
 namespace libslh::Engine {
 
-void Core::init(sf::VideoMode mode, const sf::String &title, bool &successful) {
-  _window.create(mode, title, sf::Style::Titlebar | sf::Style::Close);
-  _window.setKeyRepeatEnabled(false);
-  _window.setFramerateLimit(60);
+    void Core::init(sf::VideoMode mode, const sf::String& title,
+                    bool& successful) {
+        _window.create(mode, title, sf::Style::Titlebar | sf::Style::Close);
+        (void)ImGui::SFML::Init(_window);
+        _window.setKeyRepeatEnabled(false);
+        _window.setFramerateLimit(FRAME_LIMIT);
 
-  successful = true;
-}
-
-Core &Core::getInstance() {
-  if (_instance == nullptr) {
-    std::random_device dev;
-    _instance = new Core(dev);
-  }
-  return *_instance;
-}
-
-Core *Core::_instance{nullptr};
-
-void Core::iterate(const GameTime &gameTime, bool &successful,
-                   bool &keepRunning) {
-  keepRunning = false;
-  successful = false;
-  _sceneMan.iterate(gameTime, successful, keepRunning);
-}
-
-void Core::handleEvent(std::optional<sf::Event> event, bool &successful,
-                       bool &keepRunning) {
-  if (event->is<sf::Event::Closed>()) {
-    keepRunning = false;
-    return;
-  }
-
-  _sceneMan.handleEvent(event, successful, keepRunning);
-}
-
-void Core::run() {
-  bool keepRunning = true;
-  bool successful = true;
-  while (successful && keepRunning) {
-    iterate(_clock.newFrame(), successful, keepRunning);
-    if (!successful || !keepRunning) {
-      quit(successful);
-      return;
+        successful = true;
     }
-    while (std::optional event = _window.pollEvent()) {
-      handleEvent(event, successful, keepRunning);
-      if (!successful || !keepRunning) {
-        quit(successful);
-        return;
-      }
+
+    Core& Core::getInstance() {
+        if (_instance == nullptr) {
+            std::random_device dev;
+            _instance = new Core(dev);
+        }
+        return *_instance;
     }
-    _window.clear(_backgroundColor);
-    _window.draw(_sceneMan);
-    _window.display();
-  }
-}
 
-void Core::quit(bool /*successful*/) {}
+    Core* Core::_instance{nullptr};
 
-const sf::RenderWindow &Core::getWindow() const { return _window; }
+    void Core::iterate(const GameTime& gameTime, bool& successful,
+                       bool& keepRunning) {
+        keepRunning = false;
+        successful  = false;
+        _sceneMan.iterate(gameTime, successful, keepRunning);
+    }
 
-void Core::setNextScene(ScenePtr pScene) {
-  _sceneMan.setNextScene(std::move(pScene));
-}
+    void Core::handleEvent(std::optional<sf::Event> event, bool& successful,
+                           bool& keepRunning) {
+        if (event->is<sf::Event::Closed>()) {
+            keepRunning = false;
+            return;
+        }
 
-ScenePtr Core::getCurrentScene() const { return _sceneMan.getCurrentScene(); }
+        _sceneMan.handleEvent(event, successful, keepRunning);
+    }
 
-std::basic_string<char>
-Core::localize(const boost::locale::basic_message<char> &string) {
-  return string.str(_gen(_locale));
-}
+    void Core::run() {
+        bool keepRunning = true;
+        bool successful  = true;
+        while (successful && keepRunning) {
+            iterate(_clock.newFrame(), successful, keepRunning);
+            if (!successful || !keepRunning) {
+                quit(successful);
+                return;
+            }
+            while (std::optional event = _window.pollEvent()) {
+                handleEvent(event, successful, keepRunning);
+                if (!successful || !keepRunning) {
+                    quit(successful);
+                    return;
+                }
+            }
+            _window.clear(_backgroundColor);
+            _window.draw(_sceneMan);
+            _window.display();
+        }
+    }
+
+    void Core::quit(bool /*successful*/) {}
+
+    const sf::RenderWindow& Core::getWindow() const {
+        return _window;
+    }
+
+    void Core::setNextScene(ScenePtr pScene) {
+        _sceneMan.setNextScene(std::move(pScene));
+    }
+
+    ScenePtr Core::getCurrentScene() const {
+        return _sceneMan.getCurrentScene();
+    }
+
+    std::basic_string<char> Core::localize(
+        const boost::locale::basic_message<char>& string) {
+        return string.str(_gen(_locale));
+    }
 } // namespace libslh::Engine
